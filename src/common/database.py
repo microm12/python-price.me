b9f1a202f873aee229d173b82d2e63c52492c7b4
MAIN-PC\Mark <markosagelou@gmail.com>
2017-09-25 17:44:51 +0300

    Changed database name to the default one.

--- src/common/database.py
+++ src/common/database.py
@@ -8,7 +8,7 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.uri)
-        Database.DATABASE = client['blog']
+        Database.DATABASE = client.get_default_database()
 
     @staticmethod
     def insert(collection, data):
